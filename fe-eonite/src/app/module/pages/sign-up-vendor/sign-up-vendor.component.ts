e459de9e0e7bf81d5c499906a01f163e9cd3e75e
Jessica Ilone <jessicailonezena@gmail.com>
2024-02-25 13:58:51 +0700

    add sign up vendor form

--- fe-eonite/src/app/module/pages/sign-up-vendor/sign-up-vendor.component.ts
+++ fe-eonite/src/app/module/pages/sign-up-vendor/sign-up-vendor.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up-vendor.component.css']
 })
 export class SignUpVendorComponent {
+  phase:number = 1;
 
+  nextPhase(form: number){
+    switch (form){
+      case 1:{
+        //checkForm
+        this.phase = 2;
+        break;
+      }
+      case 2:{
+
+        this.phase = 3;
+        break;
+      }
+      case 3:{
+        //submit Form
+        break;
+      }
+    }
+
+
+  }
 }
